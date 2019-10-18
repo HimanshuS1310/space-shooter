9a98a45b1267a67156db4b8613f9875db5dc4a82
HIMANSHU SHARMA <52231652+HimanshuS1310@users.noreply.github.com>
2019-10-18 22:44:58 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <trymacro.h>
+
+#include "engine.h"
+#include "input.h"
+
+void resizeConsole()
+{
+    HWND console = GetConsoleWindow();
+    RECT r;
+    GetWindowRect(console, &r);
+    MoveWindow(console, r.left, r.top, 640, 640, TRUE);
+}
+
+mango::int32 main()
+{
+    M_TRY
+    {
+        resizeConsole();
+
+        engine::Game::game_handle game = new engine::Game();
+        game->Play();
+    }
+        M_CATCH("main.txt")
+}
+
+//M_TRY {} M_CATCH(main.txt) --> try catch block to catch 
+//mango::exception:: and critical errors and logging them to main.txt.
